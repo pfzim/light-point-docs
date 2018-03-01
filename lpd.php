fb065d77330cceaba2946169f2524ad4ee8ffcc1
Dmitry V. Zimin <pfzim@mail.ru>
2018-03-01 17:59:26 +0300

    Extend upload error info

--- lpd.php
+++ lpd.php
@@ -333,7 +333,25 @@ function php_mailer($to, $name, $subject, $html, $plain)
 				|| !file_exists(@$_FILES['file']['tmp_name'][0])
 			)
 			{
-				echo '{"code": 1, "message": "Failed upload"}';
+				$phpFileUploadErrors = array(
+					0 => 'There is no error, the file uploaded with success',
+					1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+					2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+					3 => 'The uploaded file was only partially uploaded',
+					4 => 'No file was uploaded',
+					6 => 'Missing a temporary folder',
+					7 => 'Failed to write file to disk.',
+					8 => 'A PHP extension stopped the file upload.'
+				);
+
+				if(isset($phpFileUploadErrors[intval(@$_FILES['file']['error'][0])]))
+				{
+					echo '{"code": 1, "message": "Failed upload: '.$phpFileUploadErrors[intval(@$_FILES['file']['error'][0])].'"}';
+				}
+				else
+				{
+					echo '{"code": 1, "message": "Failed upload"}';
+				}
 				exit;
 			}
 
